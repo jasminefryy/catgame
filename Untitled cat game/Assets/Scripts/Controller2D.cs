febf8d28a98434a62c1f4b62cb2224bd55d3daba
Jasmine Fry <jasminefry@me.com>
2018-02-06 12:45:53 +0000

    added background parallax and camera follows player

--- Untitled cat game/Assets/Scripts/Controller2D.cs
+++ Untitled cat game/Assets/Scripts/Controller2D.cs
@@ -3,27 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent (typeof (BoxCollider2D))]
-public class Controller2D : MonoBehaviour {
-
-	public LayerMask collisionMask;
-
-	const float skinWidth = .015f;
-	public int horizontalRayCount = 4;
-	public int verticalRayCount = 4;
+public class Controller2D : RaycastController {
 
 	float maxClimbAngle = 80;
 
-	float horizontalRaySpacing;
-	float verticalRaySpacing;
-
-	BoxCollider2D collider;
-	RaycastOrigins raycastOrigins;
 	public CollisionInfo collisions;
-
-	void Start () {
-		collider = GetComponent<BoxCollider2D> ();
-		CalculateRaySpacing ();
-	}
 
 	public void Move(Vector3 velocity) {
 		UpdateRaycastOrigins ();
@@ -115,31 +99,6 @@ public class Controller2D : MonoBehaviour {
 		}
 	}
 
-	void UpdateRaycastOrigins() {
-		Bounds bounds = collider.bounds;
-		bounds.Expand (skinWidth * -2);
-
-		raycastOrigins.bottomLeft = new Vector2 (bounds.min.x, bounds.min.y);
-		raycastOrigins.bottomRight = new Vector2 (bounds.max.x, bounds.min.y);
-		raycastOrigins.topLeft = new Vector2 (bounds.min.x, bounds.max.y);
-		raycastOrigins.topRight = new Vector2 (bounds.max.x, bounds.max.y);
-	}
-
-	void CalculateRaySpacing() {
-		Bounds bounds = collider.bounds;
-		bounds.Expand (skinWidth * -2);
-	
-		horizontalRayCount = Mathf.Clamp (horizontalRayCount, 2, int.MaxValue);
-		verticalRayCount = Mathf.Clamp (verticalRayCount, 2, int.MaxValue);	
-	
-		horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
-		verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
-	}
-
-	struct RaycastOrigins {
-		public Vector2 topLeft, topRight;
-		public Vector2 bottomLeft, bottomRight;
-	}
 
 	public struct CollisionInfo {
 		public bool above, below;
--- Untitled cat game/Assets/Scripts/scrolling_background.cs
+++ Untitled cat game/Assets/Scripts/scrolling_background.cs
@@ -4,37 +4,54 @@ using UnityEngine;
 
 public class scrolling_background : MonoBehaviour 
 {
+	public bool scrolling,paralax;
+
 	public float backgroundSize;
+	public float paralaxSpeed;
 
 	private Transform cameraTransform;
 	private Transform[] layers;
 	private float viewZone = 10;
 	private int leftIndex;
 	private int rightIndex;
+	private float lastCameraX;
 
 	private void Start()
 	{
+		
 		cameraTransform = Camera.main.transform;
+		lastCameraX = cameraTransform.position.x;
 		layers = new Transform[transform.childCount];
-		for (int i = 0; i < cameraTransform.childCount; i++)
-			layers [i] = cameraTransform.GetChild (i);
+		for(int i = 0; i < transform.childCount; i++)
+			layers[i] = transform.GetChild(i);
 
 		leftIndex = 0;
 		rightIndex = layers.Length-1;
 	}
 	private void Update()
 	{
-		if (Input.GetKeyDown (KeyCode.A))
-			ScrollLeft();
+		if (paralax) 
+		{
+			float deltaX = cameraTransform.position.x - lastCameraX;
+			transform.position += Vector3.right * (deltaX * paralaxSpeed);
+		}
 
-		if (Input.GetKeyDown (KeyCode.D))
-			ScrollRight();
+		lastCameraX = cameraTransform.position.x;
+
+		if (scrolling) 
+		{
+			if (cameraTransform.position.x < (layers [leftIndex].transform.position.x + viewZone))
+				ScrollLeft ();
+
+			if (cameraTransform.position.x > (layers [rightIndex].transform.position.x - viewZone))
+				ScrollRight ();
+		}
 	}
 
 	private void ScrollLeft()
 	{
 		int lastRight = rightIndex;
-		layers [rightIndex].position = Vector3.right * (layers[leftIndex].position.x - backgroundSize);
+		layers[rightIndex].position = Vector3.right * (layers[leftIndex].position.x - backgroundSize);
 		leftIndex = rightIndex;
 		rightIndex--;
 		if (rightIndex < 0)
@@ -44,7 +61,7 @@ public class scrolling_background : MonoBehaviour
 	private void ScrollRight()
 	{
 		int lastLeft = leftIndex;
-		layers [leftIndex].position = Vector3.right * (layers [rightIndex].position.x + backgroundSize);
+		layers[leftIndex].position = Vector3.right * (layers[rightIndex].position.x + backgroundSize);
 		rightIndex = leftIndex;
 		leftIndex++;
 		if (leftIndex == layers.Length)
